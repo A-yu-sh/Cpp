6017e54de04360827a6ef03f1c9ee4634e57b6fc
A-yu-sh <ayush.wd.dev@gmail.com>
2023-09-16 00:37:04 +0530

    Basic Cpp

---
+++ introduction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    string name;
+    cout<<"Enter Your Name : ";
+    getline(cin, name);
+    cout<<"Welcome, "<<name<<endl;
+
+    return 0;
+}
