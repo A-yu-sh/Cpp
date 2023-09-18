04aead9746b316cb8a794e4ba052486854bc7003
A-yu-sh <ayush.wd.dev@gmail.com>
2023-09-19 03:26:41 +0530

    pointer

---
+++ pointer.cpp
@@ -0,0 +1,12 @@
+ #include<iostream>
+using namespace std;
+
+int main(){
+    int num = 10;
+    int &number = num;
+    int *p;
+    p=&num;
+    cout<<number<<endl;
+    cout<<p<<endl;
+    return 0;
+}
