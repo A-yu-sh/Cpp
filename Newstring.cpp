8f16e56b532dc4699b41ca611ef2d4800ec49aa3
A-yu-sh <ayush.wd.dev@gmail.com>
2023-09-21 00:44:05 +0530

    string

---
+++ Newstring.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout<<"Enter Your Name : ";
+    getline(cin, name);
+
+    cout<<"Welcome ! "<<name;
+
+
+    return 0;
+}
